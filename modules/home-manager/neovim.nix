ac762a36afa4ffaf15caff9df8141129e5fdb605
ellis-vester <ellis_vester@protonmail.com>
2025-02-23 13:24:23 -0500

    Fixed Nvim Config - Changed home.file path for nvim config. - Updated lazy-lock.json.

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -11,7 +11,7 @@
     ];
 
     home.file = {
-      ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink ../../dotfiles/config/nvim;
+      ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/dotfiles/config/nvim";
     };
 
     home.sessionVariables = {
