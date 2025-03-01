ebb415419ef06a78ba92738380b4d054fb62b3ce
ellis-vester <ellis_vester@protonmail.com>
2025-03-01 14:37:27 -0500

    Neovim - Removed Nord Theme

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -828,9 +828,6 @@ require('lazy').setup({
     name = 'catppuccin',
     priority = 1000,
   },
-  {
-    'gbprod/nord.nvim',
-  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
