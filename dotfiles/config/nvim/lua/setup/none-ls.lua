8920dd30079b0a81602911dab9e32eb41056333e
ellis-vester <ellis_vester@protonmail.com>
2025-03-11 20:08:00 -0400

    Added format on save to none_ls for CSharp files

--- dotfiles/config/nvim/lua/setup/none-ls.lua
+++ dotfiles/config/nvim/lua/setup/none-ls.lua
@@ -7,4 +7,17 @@ null_ls.setup {
   },
 }
 
-vim.keymap.set('n', '<leader>bf', vim.lsp.buf.format, { desc = "[B]uffer [F]ormat "})
+vim.keymap.set('n', '<leader>bf', vim.lsp.buf.format, { desc = "[B]uffer [F]ormat"})
+
+vim.api.nvim_create_augroup("AutoFormat", {})
+
+vim.api.nvim_create_autocmd(
+  "BufWritePre",
+  {
+    pattern = "*.cs",
+    group = "AutoFormat",
+    callback = function()
+      vim.lsp.buf.format()
+    end,
+  }
+)
